1f3b73f1774196537a1ad5f355decaf2ca8e7f14
Artur Kukharski <arturkukharski@gmail.com>
2020-10-23 22:07:29 -0400

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const stack = [];
+    bracketsConfig = Object.fromEntries(bracketsConfig);
+
+    for (let element of str.split("")) {
+        if (stack[0] === element) {
+            stack.shift();
+        } else if (bracketsConfig[element]) {
+            stack.unshift(bracketsConfig[element]);
+        } else {
+            return false;
+        }
+    }
+
+    return !stack.length;
+};
